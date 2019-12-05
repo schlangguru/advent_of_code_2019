90c1e4529c2f21bbe975555dc2de433378e44118
Schlangguru <s.seidl@protonmail.com>
2019-12-05 18:54:00 +0100

    Implement day03 part 2

--- day03/Wire.hs
+++ day03/Wire.hs
@@ -1,10 +1,16 @@
 module Wire where
 
 import Common
+import Data.List
+import Data.Maybe
 
 type Wire = [Point]
 
 buildWire :: [Point] -> Wire
 buildWire points = scanl1 addPoint points
 
+stepsTo :: Point -> Wire -> Int
+stepsTo point wire = (fromJust $ elemIndex point wire) + 1
 
+combindedStepsTo :: Point -> [Wire] -> Int
+combindedStepsTo point wires = foldl1 (+) $ map (stepsTo point) wires
--- day03/main.hs
+++ day03/main.hs
@@ -6,11 +6,17 @@ import InputParser
 import Wire
 
 partOne :: String -> IO()
-partOne input = print $ Set.lookupMin $ Set.map (mannDist (0,0)) (Set.intersection wire1 wire2)
+partOne input = print $ Set.lookupMin $ Set.map (mannDist (0,0)) (Set.intersection (Set.fromList wire1) (Set.fromList wire2))
   where
-    [wire1, wire2] = map (Set.fromList . buildWire . parseInput) $ lines input
+    [wire1, wire2] = map (buildWire . parseInput) $ lines input
+
+partTwo :: String -> IO()
+partTwo input = let intersection = Set.intersection (Set.fromList $ wires!!0) (Set.fromList $ wires!!1) in
+  print $ Set.lookupMin $ Set.map (\point -> combindedStepsTo point wires) intersection
+  where
+    wires = map (buildWire . parseInput) $ lines input
 
 main :: IO()
 main = do
   input <- readFile "input.txt"
-  partOne input
+  partTwo input
